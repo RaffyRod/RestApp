8118fbfeca641d67a6e9a1c70482228159ea4d01
Raffy Anyelo <elraffy3@gmail.com>
2020-08-11 14:14:56 -0400

    timer added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,11 +14,25 @@ export class HomePage {
   progress: any = 0;
   minutes : number = 1;
   seconds : any = 0;
+  elapsed : any = {
+    h: '00',
+    m: '00',
+    s: '00'
+  }
 
+  overallTimer: any = false;
+  
   constructor() {}
 
 
   startTime(){
+    if(this.timer){
+      clearInterval(this.timer);
+    }
+    if(!this.overallTimer){
+      this.progressTimer();
+    }
+
     this.timer = false;
     this.percent = 0;
     this.progress = 0;
@@ -28,11 +42,32 @@ export class HomePage {
     this.seconds = timeSplit[2];
 
     let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+    if(this.percent == this.radius)
+    clearInterval(this.timer);
 
     this.timer = setInterval(()=>{
       this.percent = Math.floor((this.progress / totalSeconds) * 100);
-    })
-
+      this.progress++;
+    },1000)
   }
+  progressTimer(){
+    let countDownDate = new Date();
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+      let distance = now - countDownDate.getTime();
 
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+    }, 1000)
+  }
+  pad(num, size) {
+    let s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
 }
